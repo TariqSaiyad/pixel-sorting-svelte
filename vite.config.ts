dd2ab9007d43cc4ff88a27cfa34f8cd597ec0685
Tariq Saiyad <tariqsaiyad98@gmail.com>
2022-10-20 23:46:08 +1300

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import path from 'path';
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base:'./',
+  build:{
+    assetsDir:'./'
+
+  },
   resolve: {
     alias: {
-      $lib: path.resolve('./src/lib'),
-      $utils: path.resolve('./src/utils')
-    }
-  }
-})
+      $lib: path.resolve("./src/lib"),
+      $utils: path.resolve("./src/utils"),
+    },
+  },
+});

8a45154065c3e5b77c25bfab07373a0376c43341
Tariq Saiyad <tariqsaiyad98@gmail.com>
2022-10-20 23:01:35 +1300

    add image assets

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,6 +4,9 @@ export const MODES: Mode[] = ["BW", "ORIGINAL", "RANDOM", "SORTED"];
 export const SORT_TYPES: SortType[] = ["BRIGHTNESS", "HUE"];
 
 export const IMAGES: ImgObject[] = [
+  { title: "Beach", path: "src/assets/beach.jpg" },
+  { title: "Dunes", path: "src/assets/dunes.jpg" },
+  { title: "Waterfall", path: "src/assets/waterfall.jpg" },
   { title: "Orange Kiss", path: "src/assets/plant.jpg" },
   { title: "Mario Pixel Art", path: "src/assets/mario.png" },
   { title: "Tokyo", path: "src/assets/tokyo.jpg" },

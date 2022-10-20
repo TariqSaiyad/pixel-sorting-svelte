bb3eb803a4c4230ab63575141e05294f4edd8115
Tariq Saiyad <tariqsaiyad98@gmail.com>
2022-10-21 07:51:44 +1300

    update image paths in data file

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,11 +4,11 @@ export const MODES: Mode[] = ["BW", "ORIGINAL", "RANDOM", "SORTED"];
 export const SORT_TYPES: SortType[] = ["BRIGHTNESS", "HUE"];
 
 export const IMAGES: ImgObject[] = [
-  { title: "Beach", path: "/beach.jpg" },
-  { title: "Dunes", path: "/dunes.jpg" },
-  { title: "Waterfall", path: "/waterfall.jpg" },
-  { title: "Orange Kiss", path: "/plant.jpg" },
-  { title: "Mario Pixel Art", path: "/mario.png" },
-  { title: "Tokyo", path: "/tokyo.jpg" },
-  { title: "Venice", path: "/venice.jpg" },
+  { title: "Beach", path: "./beach.jpg" },
+  { title: "Dunes", path: "./dunes.jpg" },
+  { title: "Waterfall", path: "./waterfall.jpg" },
+  { title: "Orange Kiss", path: "./plant.jpg" },
+  { title: "Mario Pixel Art", path: "./mario.png" },
+  { title: "Tokyo", path: "./tokyo.jpg" },
+  { title: "Venice", path: "./venice.jpg" },
 ];
